b7c5aff365481ff6bf6534d781c76311f1be40ca
Teck72 <124355169+Teck72@users.noreply.github.com>
2023-02-06 15:57:42 +0100

    Add files via upload

--- streamlit_base.py
+++ streamlit_base.py
@@ -30,10 +30,23 @@ def bases_streamlit():
 
     st.sidebar.markdown("# Choix de la base")
 
-    choix = st.sidebar.radio("Choix de la base", ("Populations",  "Salaire Moyen","Etablissement"))
+    choix = st.sidebar.radio("Choix de la base", ("Projet Globale","Populations", "Salaire Moyen","Etablissement"))
 
 
     st.subheader(choix)
+    
+    
+    if choix == 'Projet Globale':
+        st.title("Les Inégalitées Salariale en France selon les terrritoires")
+        image = Image.open('./Images/SNHM.png')
+
+        st.image(image)
+        st.text("Nous pouvons constater une forte inégalité des salaires moyens selon les départements")
+        st.text("Nous allons étudier l'impacte de plusieurs variables sur celui-ci afin de fournir")
+        st.text("un outil de machine Learning capable de prédire si celui-ci sera en dessous")
+        st.text("ou supérieur au salaire Médian")
+        st.text("Nous allons aussi prédire celui-ci")
+        
 
     if choix == 'Populations' :
         st.dataframe(Popu)
@@ -59,34 +72,34 @@ def bases_streamlit():
     #Affichage des 10 départements ayant les salaires net moyen les plus élevés et bas
 
 
-    max_col = dp_salaires.head(10)
-    min_col = dp_salaires.tail(10)
+        max_col = dp_salaires.head(10)
+        min_col = dp_salaires.tail(10)
 
-    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(25,5), sharey=True)
+        fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(25,5), sharey=True)
 
-    sns.barplot(x=max_col['DEP'], y=max_col['SNHM'],ax=ax1);
-    ax1.title.set_text("10 départements ayant les salaires net moyen les plus élevés")
-    sns.barplot(x=min_col['DEP'], y=min_col['SNHM'], ax=ax2);
-    ax2.title.set_text("10 départements ayant les salaires net moyen les plus bas")
+        sns.barplot(x=max_col['DEP'], y=max_col['SNHM'],ax=ax1);
+        ax1.title.set_text("10 départements ayant les salaires net moyen les plus élevés")
+        sns.barplot(x=min_col['DEP'], y=min_col['SNHM'], ax=ax2);
+        ax2.title.set_text("10 départements ayant les salaires net moyen les plus bas")
     
-    st.write(fig)
+        st.write(fig)
     
-    fig, ax = plt.subplots(1, figsize=(15,10))
+        fig, ax = plt.subplots(1, figsize=(15,10))
     
-    dp_salaires_age = dp_salaires[['18_25ans_SNHM','26_50ans_SNHM','>50ans_SNHM']]
-    sns.boxplot(data=dp_salaires_age);
+        dp_salaires_age = dp_salaires[['18_25ans_SNHM','26_50ans_SNHM','>50ans_SNHM']]
+        sns.boxplot(data=dp_salaires_age);
     
-    st.write(fig)
+        st.write(fig)
     
-    fig, ax = plt.subplots(1, figsize=(10,10))
+        fig, ax = plt.subplots(1, figsize=(10,10))
     
-    plt.hist([dp_salaires['18_25ans_SNHM'], dp_salaires['26_50ans_SNHM'],dp_salaires['>50ans_SNHM']], bins=3, color=['red', 'blue', 'yellow'],label=['18-25', '26-50', '50+'])  
-    plt.title('Salaire moyen par heure')
-    plt.xlabel('Salaire moyen par heure')
-    plt.ylabel('Frequencies')
-    plt.legend();
+        plt.hist([dp_salaires['18_25ans_SNHM'], dp_salaires['26_50ans_SNHM'],dp_salaires['>50ans_SNHM']], bins=3, color=['red', 'blue', 'yellow'],label=['18-25', '26-50', '50+'])  
+        plt.title('Salaire moyen par heure')
+        plt.xlabel('Salaire moyen par heure')
+        plt.ylabel('Frequencies')
+        plt.legend();
     
-    st.write(fig)
+        st.write(fig)
     
     if choix == 'Etablissement' :
     
