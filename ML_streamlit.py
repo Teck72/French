e990e5afcd423a63a0c1f3fa5feb4c42d7870a3f
Teck72 <124355169+Teck72@users.noreply.github.com>
2023-02-12 13:17:18 +0100

    Add files via upload

--- ML_streamlit.py
+++ ML_streamlit.py
@@ -4,6 +4,7 @@ import streamlit as st
 import matplotlib.pyplot as plt 
 import numpy as np 
 import joblib
+import shap
 
 
 # Custom function
@@ -11,21 +12,36 @@ import joblib
 df=pd.read_csv("./Data/Data_ML.csv")
 
 
-
 def ML_stream():
     
     st.title('Machine Learning sur les salaires moyens en France')
     st.markdown('Nous allons utiliser des modéles de Régréssions pour prédir le salaire moyen d un département')
-    regr = joblib.load('RandomForestRegressor.joblib')
-    st.dataframe(df)    
-    st.subheader("Entrer le numéro de départements")
-    dep = st.text_input('', 0,95)
+    regr = joblib.load('.\Modeles\RandomForestRegressor.joblib')
+    st.dataframe(df)  
+    dep = st.selectbox('Sélectionner votre départements : ',
+    ('01 : Ain','02 : Aisne','03 : Allier','04 : Alpes-de-Haute-Provence','05 : Hautes-Alpes','06 : Alpes-Maritimes','07 : Ardèche','08 : Ardennes ','09 : Ariège',
+'10 : Aube','11 : Aude','12 : Aveyron','13 : Bouches-du-Rhône','14 : Calvados','15 : Cantal','16 : Charente','17 : Charente-Maritime','18 : Cher','19 : Corrèze',
+'2A : Corse-du-Sud','2B : Haute-Corse',"21 : Côte-d'Or","22 : Côtes-d'Armor",'23 : Creuse','24 : Dordogne','25 : Doubs','26 : Drôme','27 : Eure','28 : Eure-et-Loir',
+'29 : Finistère','30 : Gard','31 : Haute-Garonne','32 : Gers','33 : Gironde','34 : Hérault','35 : Ille-et-Vilaine','36 : Indre','37 : Indre-et-Loire','38 : Isère',
+'39 : Jura','40 : Landes','41 : Loir-et-Cher','42 : Loire','43 : Haute-Loire','44 : Loire-Atlantique','45 : Loiret','46 : Lot','47 : Lot-et-Garonne','48 : Lozère',
+'49 : Maine-et-Loire','50 : Manche','51 : Marne','52 : Haute-Marne','53 : Mayenne','54 : Meurthe-et-Moselle','55 : Meuse','56 : Morbihan','57 : Moselle','58 : Nièvre',
+'59 : Nord','60 : Oise',' 61 : Orne','62 : Pas-de-Calais','63 : Puy-de-Dôme','64 : Pyrénées-Atlantiques','65 : Hautes-Pyrénées','66 : Pyrénées-Orientales','67 : Bas-Rhin',
+'68 : Haut-Rhin','69 : Rhône','70 : Haute-Saône','71 : Saône-et-Loire','72 : Sarthe','73 : Savoie','74 : Haute-Savoie','75 : Paris','76 : Seine-Maritime','77 : Seine-et-Marne',
+'78 : Yvelines','79 : Deux-Sèvres','80 : Somme','81 : Tarn','82 : Tarn-et-Garonne','83 : Var','84 : Vaucluse','85 : Vendée','86 : Vienne','87 : Haute-Vienne','88 : Vosges',
+'89 : Yonne','90 : Territoire de Belfort','91 : Essonne','92 : Hauts-de-Seine','93 : Seine-St-Denis','94 : Val-de-Marne',"95 : Val-D'Oise"))
+    dep = dep[0:2]
     local = df[df['DEP'].isin([dep])]
     local.set_index('DEP', inplace = True)
     st.dataframe(local)
     st.subheader("Prediction du Salaire Moyen : ")
-      
     st.code(regr.predict(local))
+    explainer = shap.TreeExplainer(regr)
+    shap_values = explainer.shap_values(local)
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot(shap.summary_plot(shap_values, local, plot_type="bar"))
+    st.pyplot(shap.summary_plot(shap_values, local))
+    
+     
 
 
 ML_stream()
