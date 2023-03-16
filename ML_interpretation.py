7fdf2886b9ab7b2628af5707cbafe71c436b1d41
Teck72 <124355169+Teck72@users.noreply.github.com>
2023-03-16 08:04:15 +0100

    Add files via upload

--- ML_interpretation.py
+++ ML_interpretation.py
@@ -48,73 +48,61 @@ def ML_inter():
     df_pred = pd.DataFrame(predictions, columns=['valeur_predite'])
        
     fig, ax = plt.subplots()
+
+
     ax.plot(salaires['DEP'].head(20),salaires[selected_column].head(20), label='Valeur réelle')
     ax.plot(df_pred['valeur_predite'].head(20), label='Valeur prédite')
+
+
+    min_y, max_y = salaires[selected_column].min(), salaires[selected_column].max()
+    plt.ylim(min_y, max_y)
+
     ax.set_xlabel('Département')
     ax.set_ylabel('Salaire Horaire')
     ax.set_title('Résultats prédits')
-
-
     ax.legend()
 
-
     st.pyplot(fig)
-    
+
+
     fig, ax = plt.subplots()
     ax.plot(salaires['DEP'].iloc[20:40],salaires[selected_column].iloc[20:40], label='Valeur réelle')
     ax.plot(salaires['DEP'].iloc[20:40],df_pred['valeur_predite'].iloc[20:40], label='Valeur prédite')
+    plt.ylim(min_y, max_y)
     ax.set_xlabel('Département')
     ax.set_ylabel('Salaire Horaire')
     ax.set_title('Résultats prédits')
-
-
     ax.legend()
-
-
     st.pyplot(fig)
-    
-   
+
     fig, ax = plt.subplots()
     ax.plot(salaires['DEP'].iloc[40:60],salaires[selected_column].iloc[40:60], label='Valeur réelle')
     ax.plot(salaires['DEP'].iloc[40:60],df_pred['valeur_predite'].iloc[40:60], label='Valeur prédite')
+    plt.ylim(min_y, max_y)
     ax.set_xlabel('Département')
     ax.set_ylabel('Salaire Horaire')
     ax.set_title('Résultats prédits')
-
-
     ax.legend()
-
-
     st.pyplot(fig)
-    
-   
+
     fig, ax = plt.subplots()
     ax.plot(salaires['DEP'].iloc[60:80],salaires[selected_column].iloc[60:80], label='Valeur réelle')
     ax.plot(salaires['DEP'].iloc[60:80],df_pred['valeur_predite'].iloc[60:80], label='Valeur prédite')
+    plt.ylim(min_y, max_y)
     ax.set_xlabel('Département')
     ax.set_ylabel('Salaire Horaire')
     ax.set_title('Résultats prédits')
-
-
     ax.legend()
-
-
     st.pyplot(fig)
-    
- 
-   
+
     fig, ax = plt.subplots()
     ax.plot(salaires['DEP'].iloc[80:96],salaires[selected_column].iloc[80:96], label='Valeur réelle')
     ax.plot(salaires['DEP'].iloc[80:96],df_pred['valeur_predite'].iloc[80:96], label='Valeur prédite')
+    plt.ylim(min_y, max_y)
     ax.set_xlabel('Département')
     ax.set_ylabel('Salaire Horaire')
     ax.set_title('Résultats prédits')
-
-
     ax.legend()
-
-
     st.pyplot(fig)
-    
   
     
