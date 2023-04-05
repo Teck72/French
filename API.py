95a82076e5f90dc46a351b63312fd4d9b8a69750
Teck72 <124355169+Teck72@users.noreply.github.com>
2023-04-05 15:57:00 +0200

    Add files via upload

---
+++ API.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import requests
+import json
+
+
+def ML_api():
+    
+    st.write("Saisir les variables à envoyées à l'API :")
+    petite = st.number_input('%Petite', min_value=0, max_value=100, value=0)
+    moyenne = st.number_input('%Moyenne', min_value=0, max_value=100, value=0)
+    grande = st.number_input('%Grande', min_value=0, max_value=100, value=0)
+    aines = st.number_input('%Ainés', min_value=0, max_value=100, value=0)
+    enfants = st.number_input('%Enfants', min_value=0, max_value=100, value=0)
+    juniors = st.number_input('%Juniors', min_value=0, max_value=100, value=0)
+    masters = st.number_input('%Masters', min_value=0, max_value=100, value=0)
+    seniors = st.number_input('%Séniors', min_value=0, max_value=100, value=0)
+    indus = st.number_input('%indus', min_value=0, max_value=100, value=0)
+    const = st.number_input('%const', min_value=0, max_value=100, value=0)
+    ctrh = st.number_input('%CTRH', min_value=0, max_value=100, value=0)
+    infocomm = st.number_input('%InfoComm', min_value=0, max_value=100, value=0)
+    stservadmi = st.number_input('%STServAdmi', min_value=0, max_value=100, value=0)
+    autreserv = st.number_input('%AutreServ', min_value=0, max_value=100, value=0)
+    loyerm2 = st.number_input('Loyer/m2', min_value=0, max_value=100, value=0)
+
+
+    if st.button('Envoyer'):
+
+        data = {'%Petite': petite, '%Moyenne': moyenne, '%Grande': grande, '%Ainés': aines, '%Enfants': enfants,
+                '%Juniors': juniors, '%Masters': masters, '%Séniors': seniors, '%indus': indus, '%const': const,
+                '%CTRH': ctrh, '%InfoComm': infocomm, '%STServAdmi': stservadmi, '%AutreServ': autreserv, 'loyerm2': loyerm2}
+
+        with st.echo():
+             
+             url = 'https://teck72.pythonanywhere.com:443'
+             response = requests.post(url, json=data)
+
+
+        if response.status_code == 200:
+            # Récupération de la réponse de l'API
+            try:
+                prediction = json.loads(response.content.decode('utf-8'))['prediction']
+                st.write('La prédiction est : ', prediction)
+            except json.JSONDecodeError as e:
+                st.write('Erreur lors de l\'analyse de la réponse JSON :', e.msg)
+                st.write('Contenu de la réponse :', response.content)
+        else:
+            st.write('La requête a échoué avec le code', response.status_code)
+            st.write('Contenu de la réponse :', response.content)
+
+ML_api()    
--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import scipy
-import scipy.stats
-import time
-import altair as alt
 import seaborn as sns
-from PIL import Image
+
 
 sns.set_theme()
 
@@ -20,13 +13,16 @@ from intro import intro_streamlit
 from ML_evaluation import ML_evaluation
 from ML_predictions import ML_predi
 from ML_local import local
+from API import ML_api
+
+
+
 
 def main():
+    
+    liste_menu = ["Introduction","Visualisation Base de donnée", "Etude Machine Learning","Evaluation du modéle","Machine Learning"," Evaluation de la prédiction","Etude Local du département 33","Démonstration API","Sources"]
 
-    # List of pages
-    liste_menu = ["Introduction","Visualisation Base de donnée", "Etude Machine Learning","Evaluation du modéle","Machine Learning"," Evaluation de la prédiction","Etude Local du département 33","Sources"]
-
-    # Sidebar
+  
     menu = st.sidebar.selectbox("selectionner votre activité :", liste_menu)
 
     # Page navigation
@@ -49,7 +45,11 @@ def main():
         local()
         
     if menu == liste_menu[7] :
-         sources()    
+        ML_api()    
 
+    if menu == liste_menu[8] :
+        sources()
+        
 if __name__ == '__main__':
     main()
+  
